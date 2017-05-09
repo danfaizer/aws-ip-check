ab4515900738b2fcea58ca49938434e53ff2a19c
Daniel Beneyto <Daniel.Beneyto@schibsted.com>
2017-05-09 08:52:26 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # aws-ip-check
-Check if IP address is part of AWS infrastructure
+`aws-ip-check` is a dummy command line tool to check if an IP address belongs to Amazon Web Service (AWS) infrastructure or not.
+
+This tool relies on public [AWS IP range file](https://ip-ranges.amazonaws.com/ip-ranges.json).
+
+## Installation
+Make sure you have a working Go environment.
+
+To install `aws-ip-check` cli, simply run:
+```
+$ go get github.com/danfaizer/cmd/aws-ip-check
+```
+## Usage
+```
+$ aws-ip-check help
+usage: aws-ip-check [flags]
+  -ip string
+    	IP address to check if belongs to AWS
+  -path string
+    	File path to store AWS ip-ranges.json (default "/tmp/aws-ip-ranges.json")
+```
+
+`aws-ip-check` will return the following exit statuses:
+
+| status | description |
+| ---: | --- |
+| 0 | IP belongs to AWS IP range |
+| 1 | IP does NOT belong to AWS IP range |
+| 2 | aws-ip-check execution error |
+
+## Example
+```
+$ aws-ip-check -ip 192.168.1.1
+IP 192.168.1.1 not found in AWS ip range
+
+
+$ aws-ip-check -ip $(nslookup www.amazon.com  | grep "Address" | tail -1 | awk '{print $NF}')
+IP X.X.X.X found in AWS ip range
+```
---
+++ cmd/aws-ip-check/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+)
+
+// Prefix defines an AWS IP range
+type Prefix struct {
+	IPPrefix string `json:"ip_prefix"`
+	Region   string `json:"region"`
+	Service  string `json:"service"`
+}
+
+// AWSIPRange is read from https://ip-ranges.amazonaws.com/ip-ranges.json
+type AWSIPRange struct {
+	CreateDate string `json:"createDate"`
+	SyncToken  string `json:"syncToken"`
+	Prefixes   []Prefix
+}
+
+const (
+	rawURL = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+)
+
+func downloadFile(path string) (err error) {
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	resp, err := http.Get(rawURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func validIP(ip string) bool {
+	ipv4 := false
+	ipv6 := false
+
+	testIPv4 := net.ParseIP(ip)
+	if testIPv4.To4() != nil {
+		ipv4 = true
+	}
+
+	testIPv6 := net.ParseIP(ip)
+	if testIPv6.To16() != nil {
+		ipv6 = true
+	}
+	return ipv4 || ipv6
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: aws-ip-check [flags]")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+	ipAddress := flag.String("ip", "", "IP address to check if belongs to AWS")
+	awsIPRangeFilePath := flag.String("path", "/tmp/aws-ip-ranges.json", "File path to store AWS ip-ranges.json")
+	flag.Parse()
+	if *ipAddress == "" {
+		usage()
+		os.Exit(2)
+	}
+
+	if valid := validIP(*ipAddress); valid == false {
+		fmt.Printf("invalid IP address provided: %s\n", *ipAddress)
+		os.Exit(2)
+	}
+
+	if _, err := os.Stat(*awsIPRangeFilePath); os.IsNotExist(err) {
+		downloadFile(*awsIPRangeFilePath)
+	}
+
+	file, err := ioutil.ReadFile(*awsIPRangeFilePath)
+	if err != nil {
+		fmt.Printf("file error: %v\n", err)
+		os.Exit(2)
+	}
+
+	var ipRange AWSIPRange
+	json.Unmarshal(file, &ipRange)
+
+	for _, r := range ipRange.Prefixes {
+		_, cidrnet, err := net.ParseCIDR(r.IPPrefix)
+		if err != nil {
+			fmt.Printf("error parsing CIDR %s: %v\n", r.IPPrefix, err)
+			os.Exit(2)
+		}
+		ip := net.ParseIP(*ipAddress)
+		if cidrnet.Contains(ip) {
+			fmt.Printf("IP %s found in AWS ip range\n", *ipAddress)
+			os.Exit(0)
+		}
+	}
+	fmt.Printf("IP %s not found in AWS ip range\n", *ipAddress)
+	os.Exit(1)
+}
